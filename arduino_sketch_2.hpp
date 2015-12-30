63e24b17fb97b328fc18c25001b97f15fe6b3b47
Roger Dahl <rogerdahl@dahlsys.com>
2015-12-30 14:24:50 -0700

    initial work

--- arduino_sketch_2.hpp
+++ arduino_sketch_2.hpp
@@ -1,17 +1,14 @@
 /* This file was automatically generated.  Do not edit! */
-int wrap(int p,int w);
 u8 colorChannelClampedAdd(u8 a,u8 b);
 u32 additiveColorMix(u32 color1,u32 color2);
 u8 channelWeightedAvg(u8 a,u8 b,u8 weight);
 void colorPackedToScalar(u8 *r,u8 *g,u8 *b,u32 color);
 void addPixelColor(u16 pixelPos,u32 addColor);
 u32 colorWeightedAvg(u32 color1,u32 color2,u8 weight);
-u32 calcTaperedSegmentPixel(u8 numSegmentPixels,u8 pos,u32 centerColor);
-void drawTaperedSegment(u16 superPos,u8 numSegmentPixels,u32 color);
-u16 wrap_add(s16 v,s16 add_v,s16 max_v);
-void clear(u32 color,u16 clear_ms);
-void smoothRunners(u16 run_s,u16 delay_ms,struct Segment *segmentPtrIn,u8 numSegments);
-void xmasRedGreenTwinkles(u16 run_s);
+u32 calcTailSegmentPixel(u8 numSegmentPixels,u8 pos,u32 startColor);
+void drawTaperedSegment(u16 superPos,u8 numSegmentPixels,u32 color,u8 numEnterExitPixels);
+void clear();
+void meteors(u16 runSec,u16 delayMs,u8 numMeteors,u8 minPixels,u8 maxPixels,s16 minSpeed,s16 maxSpeed);
 void loop();
 void setup();
 extern Adafruit_NeoPixel pixels;
